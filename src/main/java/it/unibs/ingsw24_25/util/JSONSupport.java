cd1403dc36d0374c10f1c9e7a96a4b4681458fde
andreasberna <93040971+andreasberna@users.noreply.github.com>
2025-10-01 16:46:36 +0200

    feat(repository): creazione classe JSONSupport per la gestione delle mappe in repository

---
+++ src/main/java/it/unibs/ingsw24_25/util/JSONSupport.java
@@ -0,0 +1,61 @@
+package it.unibs.ingsw24_25.util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import it.unibs.ingsw24_25.model.*;
+
+import java.lang.reflect.Type;
+import java.util.Map;
+
+public final class JSONSupport {
+
+    private static final Gson GSON = new GsonBuilder()
+            .setPrettyPrinting()
+            .create();
+
+    // Tipi per la (de)serializzazione delle mappe
+    private static final Type VOL_MAP_TYPE = new TypeToken<Map<String, Volunteer>>(){}.getType();
+    private static final Type PLACE_MAP_TYPE = new TypeToken<Map<String, Place>>(){}.getType();
+    private static final Type VISIT_MAP_TYPE = new TypeToken<Map<String, VisitType>>(){}.getType();
+
+    private JSONSupport() {
+        // Costruttore privato: classe utility
+    }
+
+    // ---------- Volunteer ----------
+    public static String serializeVolunteerMap(Map<String, Volunteer> map) {
+        return GSON.toJson(map, VOL_MAP_TYPE);
+    }
+
+    public static Map<String, Volunteer> deserializeVolunteerMap(String json) {
+        return GSON.fromJson(json, VOL_MAP_TYPE);
+    }
+
+    // ---------- Place ----------
+    public static String serializePlaceMap(Map<String, Place> map) {
+        return GSON.toJson(map, PLACE_MAP_TYPE);
+    }
+
+    public static Map<String, Place> deserializePlaceMap(String json) {
+        return GSON.fromJson(json, PLACE_MAP_TYPE);
+    }
+
+    // ---------- VisitType ----------
+    public static String serializeVisitTypeMap(Map<String, VisitType> map) {
+        return GSON.toJson(map, VISIT_MAP_TYPE);
+    }
+
+    public static Map<String, VisitType> deserializeVisitTypeMap(String json) {
+        return GSON.fromJson(json, VISIT_MAP_TYPE);
+    }
+
+    // ---------- SystemSettings (singolo oggetto) ----------
+    public static String serializeSystemSettings(SystemSettings settings) {
+        return GSON.toJson(settings);
+    }
+
+    public static SystemSettings deserializeSystemSettings(String json) {
+        return GSON.fromJson(json, SystemSettings.class);
+    }
+}
